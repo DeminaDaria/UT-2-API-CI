d237af7bd5d700e5d3d768ed32b0910209ad8e10
DeminaDaria <61200143+DeminaDaria@users.noreply.github.com>
2021-02-26 23:15:07 +0300

    added check for accounts.schema file

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -3,6 +3,7 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 class MobileBankApiTestV1 {
     @Test
@@ -16,6 +17,8 @@ class MobileBankApiTestV1 {
           .get("/demo/accounts")
       // Проверки
       .then()
-          .statusCode(200);
+          .statusCode(200)
+          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+      ;
     }
 }
